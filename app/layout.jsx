291472eaec85f275f80cd6a58ed51ba57d9c8a49
Luke Satchwell <108826672+Luke-Satchwell@users.noreply.github.com>
2023-05-14 23:50:38 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} max-h-screen`}>
+      <body className={`${inter.className} h-screen`}>
         <Provider>
           <Nav />
           <main>{children}</main>
