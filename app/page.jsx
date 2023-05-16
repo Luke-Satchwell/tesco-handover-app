7397e8abb39df507e80bb3f415b2c711d8588c9f
Luke Satchwell <108826672+Luke-Satchwell@users.noreply.github.com>
2023-05-16 06:34:30 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Feed from "@/components/Feed";
 
 const Home = () => {
   return (
-    <section className="w-full flex items-center flex-col gap-8 md:gap-14 px-4">
+    <section className="w-full flex items-center flex-col gap-8 md:gap-14 px-4 pb-[4rem]">
       <h1 className="text-4xl md:text-5xl text-center font-bold gradient-heading">
         Create and Share <br className="max-md:hidden" />
         <span className="text-center">Your Handover</span>
